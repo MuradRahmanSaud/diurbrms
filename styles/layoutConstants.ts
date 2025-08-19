c210207b2ab54c70322b15282fbc08337cdd8163
Murad Rahman Saud <98799132+MuradRahmanSaud@users.noreply.github.com>
2025-08-19 15:26:08 +0600

    V-03

--- styles/layoutConstants.ts
+++ styles/layoutConstants.ts
@@ -1,4 +1,4 @@
 // styles/layoutConstants.ts
-export const SHARED_SIDE_PANEL_WIDTH_CLASSES = "w-60 sm:w-64 md:w-72 lg:w-80 xl:w-96";
+export const SHARED_SIDE_PANEL_WIDTH_CLASSES = "w-60 sm:w-64 md:w-72 lg:w-80 xl:w-80 2xl:w-96";
 export const SIDEBAR_FOOTER_HEIGHT_PX = 36;
 export const SIDEBAR_FOOTER_PADDING_BOTTOM_CLASS = 'pb-9'; // pb-9 = 2.25rem = 36px, matches footer height
